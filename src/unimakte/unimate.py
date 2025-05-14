e82a8310ae2f1f79518a672dc503021176e1f3ef
Wasif Ali Raza <wasifaliraza95@gmail.com>
2025-05-14 08:41:46 -0700

    added some changes but still have area of debuggging

--- src/unimakte/unimate.py
+++ src/unimakte/unimate.py
@@ -1,42 +1,43 @@
-from crewai import Crew,process,task
-from crewai.project import CrewBase,agent,crew,task
+from crewai import Crew,Process,task ,agent
+from crewai.project import CrewBase,agent,crew ,task
 from dotenv import load_dotenv
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
 
 load_dotenv()
 
-@crew
+@CrewBase
 class Unimate():
-    agents_config='config/unimate/agents.yaml'
-    task_config='config/unimate/task.yaml'
+    # agents_config='config/agents.yaml'
+    # task_config='config/task.yaml'
 
+    agent:list[BaseAgent]
+    task:list[task]
     @agent
-    def Task_magner():
-        return agent(
-            config=agents_config,
-            verbose=true,
-            memory=true
-        )
+    def task_manager(self):
+        pass
     
     @agent
-    def study_assistant():
-        return agent(
-            config=agents_config,
-            verbose=true,
-            memory=true
-        )
+    def study_assistant(self):
+        pass
     
     @agent
-    def note_organizer():
-        return agent(
-            config=agents_config,
-            verbose=true,
-            memory=true
-        )
+    def note_organizer(self):
+      pass
     
     @agent 
-    def content_gernator():
-        return agent(
-            config=agents_config,
-            verbose=true,
-            memory=true
-        )
+    def content_generator(self):
+      pass
+    
+    @crew
+    def crew(self)->Crew:
+       return Crew(
+          agents=self.agents,
+          tasks=self.tasks,
+          process=Process.sequential,
+          verbose=True
+       )
+    
+def uni_mate():
+    result= Unimate().crew().kickoff()   
+    print(result)
